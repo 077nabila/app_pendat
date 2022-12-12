bbec9a2eae35e7d7a77ad02d69b6be1707841c13
Nabila Regitasyari <84031548+077nabila@users.noreply.github.com>
2022-12-12 11:19:47 +0700

    Update Profil.py

--- Profil.py
+++ Profil.py
@@ -9,21 +9,13 @@ from sklearn.utils.validation import joblib
 # px.defaults.color_continuous_scale = "reds"
 
 # create content
-st.markdown("# Profil")
 st.title("Profil Saya")
 st.container()
 st.write(''' 
             Nama : Nabila Regitasyari Irmawan
             \nNim  : 200411100077
-            
-            \n**Pengertian Data Mining**
-            \ndata mining adalah proses pengumpulan dan pengolahan data yang bertujuan untuk mengekstrak informasi penting pada data.
-
-            \nData mining memiliki tiga tujuan yaitu sebagai sarana untuk menjelaskan atau explanatory, untuk konfirmasi atau confirmatory, dan untuk eksplorasi atau exploratory.
-
-            \nData mining juga memiliki beberapa metode seperti Association, Classification, Regression, dan Clustering.
-
-        ''')
+            /nKelas : Penambangan Data B
+           
 
 
 
